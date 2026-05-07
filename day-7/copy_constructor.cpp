f845a87e8ee6512d88d4a98049d2828dd02d3b30
Divesh Funde <diveshfunde17@gmail.com>
2026-05-07 09:48:25 +0530

    normal commit

---
+++ day-7/copy_constructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Player{
+    public:
+    string name;
+    int age;
+    void setName (string n){
+        name =n
+    }
+};
+
+class cricketer : public Player{
+    int runs;
+    void setruns(int r){
+        runs=4;
+    }
+    void show(){
+
+        cout<<"Name:"<<
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+int main(){
+
+
+    return 0;
+}
